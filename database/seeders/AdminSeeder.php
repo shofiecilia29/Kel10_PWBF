acd3e5320ba94b064340b0b1b89020f716a3bb4e
shofiecilia29 <shofie.cilia.hermawan-2022@vokasi.unair.ac.id>
2023-07-19 15:40:09 +0700

    bissmillah

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $admin = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('123'),
+                'is_admin' => true,
+            ]
+            ];
+        User::insert($admin);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[\App\Http\Controllers\Homecontroller::class, 'index'])->name('homepage');
+Route::get('/shop',[\App\Http\Controllers\Shopcontroller::class, 'index'])->name('shop.index');
+Route::get('/product',[\App\Http\Controllers\Productcontroller::class, 'show'])->name('product.show');
+
+Route::resource('/cart',\App\Http\Controllers\CartController::class);
+
+Route::get('/order/checkout',[\App\Http\Controllers\OrderController::class, 'process'])->name('checkout.process');
+
+Route::group(['middleware' => ['auth','isAdmin'],'prefix' => 'admin'], function(){
+    Route::get('/',[\App\Http\Controllers\Admin\DashboardController::class,'index'])->name('dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
